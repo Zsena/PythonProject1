ddfb3bb10b9949289d61828625c2d2aebca3b163
zsena <zsanett.tamas87@gmail.com>
2025-09-14 23:51:17 +0200

    name and neptun code added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# TAMÁS ZSANETT - Q4PTDR
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
